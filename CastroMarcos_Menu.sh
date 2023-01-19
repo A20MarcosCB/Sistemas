53c75bb8f3716dd0c1fc52951ccd0187530b03ea
A20MarcosCB <114486611+A20MarcosCB@users.noreply.github.com>
2023-01-19 13:58:45 +0100

    Add files via upload

--- CastroMarcos_Menu.sh
+++ CastroMarcos_Menu.sh
@@ -16,11 +16,20 @@ function f.menu(){
         echo "2. Realizar una suma"
         echo "3. Realizar una resta"
         echo "4. Identificar el procesador del equipo."
+        echo "5. Crear notas."
 }
 function f.identificar(){
+    clear
     echo ""
-                echo "IDENTIFICAR ARCHIVOS DEL HOME DEL USUARIO"
-                echo ""
+    echo ██╗░░██╗░█████╗░███╗░░░███╗███████╗
+    echo ██║░░██║██╔══██╗████╗░████║██╔════╝
+    echo ███████║██║░░██║██╔████╔██║█████╗░░
+    echo ██╔══██║██║░░██║██║╚██╔╝██║██╔══╝░░
+    echo ██║░░██║╚█████╔╝██║░╚═╝░██║███████╗
+    echo ╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚═╝╚══════╝
+    echo ""
+    echo "IDENTIFICAR ARCHIVOS DEL HOME DEL USUARIO"
+    echo ""
                 for file in $HOME/*
                     do
                         if [ -d $file ]
@@ -33,11 +42,20 @@ function f.identificar(){
                             echo $file non é nin un ficheiro nin un directorio.
                         fi
                     done
+    echo ""
 }
 function f.suma(){
+    clear
     echo ""
-                echo "SUMA"
-                echo ""
+    echo ░██████╗██╗░░░██╗███╗░░░███╗░█████╗░
+    echo ██╔════╝██║░░░██║████╗░████║██╔══██╗
+    echo ╚█████╗░██║░░░██║██╔████╔██║███████║
+    echo  ░╚═══██╗██║░░░██║██║╚██╔╝██║██╔══██║
+    echo ██████╔╝╚██████╔╝██║░╚═╝░██║██║░░██║
+    echo ╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝
+    echo ""
+    echo "REALIZAR UNA SUMA"
+    echo ""
                 x=0
                 y=0
                 read -p "Introduce el primer número: " x
@@ -46,18 +64,67 @@ function f.suma(){
                 echo "El resultado es: " $resultado
 }
 function f.resta(){
+    clear
     echo ""
-                echo "RESTA"
-                echo ""
-                x=0
-                y=0
-                read -p "Introduce el primer número: " x
-                read -p "Introduce el segundo número: " y
-                resultado=$(($x - $y))
-                echo "El resultado es: " $resultado
+    echo ██████╗░███████╗░██████╗████████╗░█████╗░
+    echo ██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗
+    echo ██████╔╝█████╗░░╚█████╗░░░░██║░░░███████║
+    echo ██╔══██╗██╔══╝░░░╚═══██╗░░░██║░░░██╔══██║
+    echo ██║░░██║███████╗██████╔╝░░░██║░░░██║░░██║
+    echo ╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═
+    echo ""
+    echo "REALIZAR UNA RESTA"
+    echo ""
+    x=0
+    y=0
+    read -p "Introduce el primer número: " x
+    read -p "Introduce el segundo número: " y
+    resultado=$(($x - $y))
+    echo "El resultado es: " $resultado
 }
 function f.procesador(){
+    clear
+    echo ""
+    echo ░█████╗░██████╗░██╗░░░██╗
+    echo ██╔══██╗██╔══██╗██║░░░██║
+    echo ██║░░╚═╝██████╔╝██║░░░██║
+    echo ██║░░██╗██╔═══╝░██║░░░██║
+    echo ╚█████╔╝██║░░░░░╚██████╔╝
+    echo ░╚════╝░╚═╝░░░░░░╚═════╝░
+    echo ""
+    echo "IDENTIFICAR EL PROCESADOR DEL EQUIPO"
+    echo ""
     grep -w Modelo lscpu
+}
+function f.notas(){
+            clear
+            echo ""
+            echo ███╗░░██╗░█████╗░████████╗░█████╗░░██████╗
+            echo ████╗░██║██╔══██╗╚══██╔══╝██╔══██╗██╔════╝
+            echo ██╔██╗██║██║░░██║░░░██║░░░███████║╚█████╗░
+            echo ██║╚████║██║░░██║░░░██║░░░██╔══██║░╚═══██╗
+            echo ██║░╚███║╚█████╔╝░░░██║░░░██║░░██║██████╔╝
+            echo ╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░
+            echo ""
+            echo "CREAR NOTAS"
+            echo ""
+            read -p "Nombre de la nota (sin extensión): " nota
+            if [ -e Notas ]
+                then
+                    echo
+                else
+                    mkdir Notas
+            fi
+            if [ -e Notas/$nota.txt ]
+                then
+                    echo "Nombre ya existente."
+                    echo ""
+                else
+                    touch Notas/$nota.txt
+                    read -p "Contenido de la nota: " contenido
+                    echo $contenido > Notas/$nota.txt
+                    echo ""
+            fi
 }
 until ((sair==0))
     do
@@ -81,6 +148,9 @@ until ((sair==0))
             4)
                 f.procesador
                 ;;
+            5)
+                f.notas
+                ;;
             *)
                 clear
                 echo ╭━━━╮
@@ -89,7 +159,9 @@ until ((sair==0))
                 echo ┃╭━━┫╭┫╭┫╭╮┃╭╯
                 echo ┃╰━━┫┃┃┃┃╰╯┃┃
                 echo ╰━━━┻╯╰╯╰━━┻╯
+                echo ""
                 echo "Selecciona una de las anteriores opciones."
+                echo ""
                 ;;
         esac
     #Amosar unha condición de saída do programa
